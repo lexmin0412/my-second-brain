acae66eea36695a0f3a6976438a6adb942b2c6d0
lexmin0412 <zhangle_dev@outlook.com>
2023-11-01 22:22:45 +0800

    feat(utils/oss): 支持复制/删除/重命名文件

--- src/utils/oss.tsx
+++ src/utils/oss.tsx
@@ -34,15 +34,37 @@ class OssClient {
   put = (fileName: string, content: string) => {
     return this.store.put(
       `/apis/my-second-brain/articles/${fileName}.md`,
-			// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-			// @ts-ignore
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
       new OSS.Buffer(content)
     );
   };
 
-	get = (fileName: string) => {
-		return this.store.get(`/apis/my-second-brain/articles/${fileName}.md`)
-	}
+  get = (fileName: string) => {
+    return this.store.get(`apis/my-second-brain/articles/${fileName}.md`);
+  };
+
+  copy = (sourceFileName: string, targetFileName: string) => {
+    return this.store.copy(
+      `apis/my-second-brain/articles/${targetFileName}.md`,
+      `apis/my-second-brain/articles/${sourceFileName}.md`
+    );
+  };
+
+  delete = (fileName: string) => {
+    return this.store.delete(`/apis/my-second-brain/articles/${fileName}.md`);
+  };
+
+  rename = async(sourceFileName: string, targetFileName: string) => {
+    await this.copy(
+      sourceFileName,
+      targetFileName,
+    );
+		const res = await this.delete(sourceFileName);
+		return Promise.resolve({
+      res,
+    });
+  };
 }
 
 export default OssClient;
