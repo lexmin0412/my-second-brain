9c115e9fe8bfdc42a8064e0af61b130d4a196c2a
lexmin0412 <zhangle_dev@outlook.com>
2023-11-02 13:54:41 +0800

    feat: 引入 rehype-highlight 和 rehype-raw, 支持代码高亮、HTML、Github Flavored Markdown 样式

--- src/home/components/editor.tsx
+++ src/home/components/editor.tsx
@@ -1,34 +1,35 @@
 import {ChangeEvent, useContext, useEffect, useState} from "react";
-import { message, Modal, Spin } from 'antd'
+import {message, Modal, Spin} from "antd";
 import Markdown from "react-markdown";
-import reactGFM from 'remark-gfm'
+import reactGFM from "remark-gfm";
+import rehypeHighlight from "rehype-highlight";
+import rehypeRaw from 'rehype-raw'
 import PublishConfirmModal from "./publish-confirm-modal";
-import { GlobalContext } from "@/hooks/context";
+import {GlobalContext} from "@/hooks/context";
 
 interface EditorProps {
-	/**
-	 * 编辑器 loading
-	 */
-	loading?: boolean
+  /**
+   * 编辑器 loading
+   */
+  loading?: boolean;
   /**
    * 初始值
    */
   initialContent: string;
-	/**
-	 * 发布成功回调
-	 */
-  onPublishSuccess: () => void
+  /**
+   * 发布成功回调
+   */
+  onPublishSuccess: () => void;
 }
 
 export default function Editor(props: EditorProps) {
-
-	const {loading, initialContent, onPublishSuccess} = props;
+  const {loading, initialContent, onPublishSuccess} = props;
 
   const [content, setContent] = useState("");
-	const [publishConfirmModalOpen, setPublishConfirmModalOpen] = useState(false);
-	const { ossClient, selectedSidebarItem } = useContext(GlobalContext)
+  const [publishConfirmModalOpen, setPublishConfirmModalOpen] = useState(false);
+  const {ossClient, selectedSidebarItem} = useContext(GlobalContext);
 
-	useEffect(() => {
+  useEffect(() => {
     setContent(initialContent);
   }, [initialContent]);
 
@@ -36,50 +37,55 @@ export default function Editor(props: EditorProps) {
     setContent(e.target.value);
   };
 
-	/**
-	 * 更新文档
-	 */
-	const handleDocUpdate = async(values: {fileName: string}) => {
+  /**
+   * 更新文档
+   */
+  const handleDocUpdate = async (values: {fileName: string}) => {
     await ossClient?.put(values.fileName, content);
-		message.success('发布成功')
-		setPublishConfirmModalOpen(false)
-		onPublishSuccess()
+    message.success("发布成功");
+    setPublishConfirmModalOpen(false);
+    onPublishSuccess();
   };
 
-	/**
-	 * 发布按钮点击
-	 */
-	const handlePublishBtnClick = async() => {
-		if (selectedSidebarItem) {
-			Modal.confirm({
-				title: '发布确认',
-				onOk: () => {
-					return handleDocUpdate({
+  /**
+   * 发布按钮点击
+   */
+  const handlePublishBtnClick = async () => {
+    if (selectedSidebarItem) {
+      Modal.confirm({
+        title: "发布确认",
+        onOk: () => {
+          return handleDocUpdate({
             fileName: selectedSidebarItem.title,
           });
-				}
-			})
-		} else {
-			setPublishConfirmModalOpen(true)
-		}
-	}
+        },
+      });
+    } else {
+      setPublishConfirmModalOpen(true);
+    }
+  };
 
-	// 语法插件
-	const remarkPlugins = [reactGFM];
+  // 语法插件
+  const remarkPlugins = [reactGFM];
+  // 高亮插件
+  const rehypePlugins = [rehypeRaw, rehypeHighlight];
 
   return (
     <Spin spinning={loading} className="tsb-spin">
       <div className="flex w-full h-full box-border relative">
-				{/* 编辑区域 */}
+        {/* 编辑区域 */}
         <textarea
           value={content}
           placeholder="请输入内容开始编辑"
           className="flex-1 border-0 w-full h-full block outline-none border-r border-solid border-r-[#eff0f5] resize-none p-4 box-border text-base"
           onChange={handleChange}
         />
-
-				{/* 预览区域 */}
-        <Markdown className="flex-1 p-4 overflow-auto" remarkPlugins={remarkPlugins}>
+        {/* 预览区域 */}
+        <Markdown
+          className="flex-1 p-4 overflow-auto markdown-body"
+          remarkPlugins={remarkPlugins}
+          rehypePlugins={rehypePlugins}
+        >
           {content}
         </Markdown>
         <div className="absolute top-4 right-4">
