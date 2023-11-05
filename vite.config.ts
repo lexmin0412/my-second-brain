60760e0d56b7eeb36f31f20c8af7f35661c1b99c
lexmin0412 <zhangle_dev@outlook.com>
2023-11-05 20:24:55 +0800

    chore: 引入 @vitejs/plugin-legacy 实现低版本浏览器兼容

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import * as path from 'path'
 import react from '@vitejs/plugin-react-swc'
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,5 +11,10 @@ export default defineConfig({
 			'@': path.resolve(__dirname, './', 'src'),
 		}
 	},
-  plugins: [react()],
+  plugins: [
+		react(),
+		legacy({
+			targets: ['defaults', 'not IE 11'],
+		}),
+	],
 })
