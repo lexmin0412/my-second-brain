874b940053537b4b254d707da55165825aeab39e
lexmin0412 <zhangle_dev@outlook.com>
2023-11-07 13:56:30 +0800

    feat: 新增 replaceName 和 getNameWithoutSuffix util

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -1,2 +1,18 @@
 export * from './oss'
 export * from './device'
+
+/**
+ * 替换字符串中的指定字符串
+ */
+export const replaceName = (str: string, replaceStr: string, targetStr: string) => {
+  const arr = str.split("");
+  arr.splice(str.lastIndexOf(`/${replaceStr}`)+1, replaceStr.length, targetStr);
+  return arr.join("");
+};
+
+/**
+ * 获取去除后缀的文件名
+ */
+export const getNameWithoutSuffix = (originalName: string) => {
+  return originalName.slice(0, originalName.lastIndexOf(".md"));
+};
